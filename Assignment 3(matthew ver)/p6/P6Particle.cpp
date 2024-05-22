2d4e10773677df7bd819f2c4716f5232ac5ffa6d
Matthew Jayd Baldonado <matthew_baldonado@dlsu.edu.ph>
2024-05-22 16:23:09 +0800

    working

---
+++ Assignment 3(matthew ver)/p6/P6Particle.cpp
@@ -0,0 +1,16 @@
+#include "P6Particle.h"
+
+using namespace P6;
+
+void P6Particle::UpdatePosition(float time) {
+	//p2 = p1 + v t
+	//Position = Position + Velocity * time;
+}
+
+void P6Particle::UpdateVelocity(float time) {
+
+}
+
+void P6Particle::update(float time) {
+
+}
